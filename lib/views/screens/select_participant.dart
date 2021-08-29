af315e557ee5d327566a5ac28856c13301211f66
Umair Jibran <umairjibran7@gmail.com>
2021-08-29 23:32:49 +0500

    add participant selection screen

---
+++ lib/views/screens/select_participant.dart
@@ -0,0 +1,96 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+class SelectParticipant extends StatefulWidget {
+  static final String routeName = "/participant-selection";
+  const SelectParticipant({Key? key}) : super(key: key);
+
+  @override
+  _SelectParticipantState createState() => _SelectParticipantState();
+}
+
+class _SelectParticipantState extends State<SelectParticipant> {
+  var loadParticipants;
+
+  Future<List<String?>>? fetchParticipants() async {
+    FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    QuerySnapshot<Map<String, dynamic>> allUsers =
+        await _firestore.collection("users").get();
+    List<String?> userIds = [];
+    allUsers.docs.forEach((user) {
+      if (user.id != FirebaseAuth.instance.currentUser?.uid)
+        userIds.add(user.id);
+      // if user has the number saved do not push it to the array `userIds`
+    });
+    return userIds;
+  }
+
+  Future<void> beginConversation(String otherUserId) async {
+    FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    var conversation = await _firestore.collection("chats").where(
+        "chatParticipants",
+        isEqualTo: [FirebaseAuth.instance.currentUser?.uid, otherUserId]).get();
+    if (conversation.docs.isEmpty) {
+      await _firestore.collection("chats").doc().set({
+        "chatParticipants": [
+          FirebaseAuth.instance.currentUser?.uid,
+          otherUserId
+        ],
+        "conversationStartedBy": FirebaseAuth.instance.currentUser?.uid,
+        "lastMessage": "Just Started",
+        "lastMessageTime": DateTime.now().millisecondsSinceEpoch,
+        "lastMessageSender": FirebaseAuth.instance.currentUser?.uid,
+      });
+    }
+    Navigator.of(context).pushNamed(
+      ChatScreenView.routeName,
+      arguments: {
+        "participants": [FirebaseAuth.instance.currentUser?.uid, otherUserId]
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    loadParticipants = fetchParticipants();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Select Participants"),
+      ),
+      body: FutureBuilder(
+        future: loadParticipants,
+        builder: (_, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              List<String?> userIds = snapshot.data;
+              return ListView.builder(
+                itemCount: userIds.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(userIds[index] ?? ""),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 20,
+                    ),
+                    onTap: () => beginConversation(userIds[index] ?? ""),
+                  );
+                },
+              );
+            }
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
