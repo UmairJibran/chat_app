20cd6f36a32db1133aa2f8ae8b1807be597cbca6
Umair Jibran <umairjibran7@gmail.com>
2021-08-31 01:37:18 +0500

    showing conversations continuously as it happens

--- lib/views/screens/chat_screen.dart
+++ lib/views/screens/chat_screen.dart
@@ -1,25 +1,76 @@
+import 'package:chat_app/views/widgets/message_bubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ChatScreenView extends StatefulWidget {
   static const String routeName = "chat-screen";
+  const ChatScreenView(this.chatId, {Key? key}) : super(key: key);
+
+  final String? chatId;
 
   @override
   _ChatScreenViewState createState() => _ChatScreenViewState();
 }
 
 class _ChatScreenViewState extends State<ChatScreenView> {
-  var participants;
+  var userId;
+  var getAllConverations;
+
+  Stream<QuerySnapshot> fetchConversation() {
+    FirebaseFirestore _firestore = FirebaseFirestore.instance;
+    Stream<QuerySnapshot<Map<String, dynamic>>> querySnapshot = _firestore
+        .collection("chats")
+        .doc(widget.chatId)
+        .collection("conversation")
+        .orderBy('timestamp', descending: false)
+        .snapshots();
+    return querySnapshot;
+  }
+
+  @override
+  initState() {
+    getAllConverations = fetchConversation();
+    userId = FirebaseAuth.instance.currentUser?.uid;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    participants = ModalRoute.of(context)!.settings.arguments;
-    print(participants);
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Center(
-          child: Text(participants["participants"].toString()),
-        ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: getAllConverations,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print("error: " + snapshot.error.toString());
+            return Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return LinearProgressIndicator();
+          }
+          List<DocumentSnapshot> docs = snapshot.data!.docs;
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 05, horizontal: 20),
+            child: ListView.builder(
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
+                String? content = docs[index]["content"];
+                int? timestamp = docs[index]["timestamp"];
+                String? sender = docs[index]["sender"];
+                return MessageBubble(
+                  isMe: sender == userId,
+                  time: DateFormat.Hm().format(
+                    DateTime.fromMillisecondsSinceEpoch(timestamp!),
+                  ),
+                  message: content,
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
